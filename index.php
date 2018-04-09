a01cc16dd2898fc0c206c236d97ccfd89f727d3e
Matthew.ekpoto <idomatthew@gmail.com>
2018-04-09 07:47:54 +0100

    hngtest

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #0000FF;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111111;
+}
+
+.c{
+ padding:5px;
+ margin-left:800px;
+ margin-top:10px;
+ margin-button:5px;
+}
+
+</style>
+</head>
+<body>
+
+<div>
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+   <li> <a href <input type="text" placeholder="Search.."> </a> </li>  
+   <li><input class="c" type="text" placeholder="search..">search</li>
+   
+</ul>
+</div>
+
+<?php
+echo "<br>";
+echo "hello world!";
+echo "<br>";
+echo "The time is " . date("h:i:sa"); "<br>";
+?>
+ 
+
+
+
+
+
+
+
+<div> 
+
+</div>
+
+</body>
+</html>
